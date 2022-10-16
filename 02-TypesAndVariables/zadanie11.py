a5610ce1788c9708cd4a6f61ea808c2c4a3b100b
Krystian <226311@student.uek.krakow.pl>
2022-10-16 18:23:28 +0200

    Dodano zadanie 11 i zadanie 12

---
+++ 02-TypesAndVariables/zadanie11.py
@@ -0,0 +1,6 @@
+# My name is ..., I’m ... years old, and my height is ... cm 
+
+name = 'Krystian'
+age = 19
+height = 180
+print(f"My name is {name}, I'm {age} years old, and my height is {height} cm.")
---
+++ 02-TypesAndVariables/zadanie12.py
@@ -0,0 +1,6 @@
+# The value is …, and … is its second power is … 
+
+value = 5
+power = value**2
+
+print("The value is {0}, and {0} is its second power is {1}".format(value, power))
