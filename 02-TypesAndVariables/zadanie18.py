3c238d2bd3e32fc3248000582f6c180892c4c84d
Krystian <226311@student.uek.krakow.pl>
2022-10-16 20:19:18 +0200

    CDodano zadanie 18

---
+++ 02-TypesAndVariables/zadanie18.py
@@ -0,0 +1,14 @@
+# Area of the Triangle using Heron's Formula 
+from math import sqrt
+
+a,b,c = (input("Enther the length of the sides of the triangle: \n")).split()
+a = float(a)
+b = float(b)
+c = float(c)
+
+p = (a+b+c)/2
+S = sqrt(p*(p-a)*(p-b)*(p-c))
+S = round(S, 2)
+
+# print("The area of the triangle is {0}".format(S))
+print(f"The area of the triangle is {S}")
