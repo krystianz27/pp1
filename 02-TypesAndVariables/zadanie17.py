e9dc4e82b4187c4ea9facea63e91d512ca7b4511
Krystian <226311@student.uek.krakow.pl>
2022-10-16 20:00:37 +0200

    Commit zadania 17

---
+++ 02-TypesAndVariables/zadanie17.py
@@ -0,0 +1,7 @@
+# I am 170cm tall, i.e. 5 feet and 7 inches.
+
+tall = 180
+feet = round(tall / 30.48, 2)
+inches = round(tall / 2.54, 2)
+
+print("I am 180 cm tall, i.e {0} feet and {1} inches".format(feet, inches))
