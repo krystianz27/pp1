fb1de29d94acc65e5b80dbe18ce75685804d05b4
Krystian <226311@student.uek.krakow.pl>
2022-10-16 18:32:13 +0200

    Commit zadanie11

--- 02-TypesAndVariables/zadanie12.py
+++ 02-TypesAndVariables/zadanie12.py
@@ -4,3 +4,5 @@ value = 5
 power = value**2
 
 print("The value is {0}, and {0} is its second power is {1}".format(value, power))
+
+print("The value is {0}, and {0} is its second power is {1}".format(value, power))
